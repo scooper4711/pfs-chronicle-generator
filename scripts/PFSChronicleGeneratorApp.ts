18ef1c46fec34ffa4fc994c56b0488299e45a036
Stephen Cooper <scooper4711@users.noreply.github.com>
2025-08-16 23:49:23 -0400

    feat: ready to start looking at PDF generation

--- scripts/PFSChronicleGeneratorApp.ts
+++ scripts/PFSChronicleGeneratorApp.ts
@@ -1,11 +1,22 @@
-
 import ApplicationV2 = foundry.applications.api.ApplicationV2;
 import HandlebarsApplicationMixin = foundry.applications.api.HandlebarsApplicationMixin;
 import FormDataExtended = foundry.applications.ux.FormDataExtended;
 
-
-
 export class PFSChronicleGeneratorApp extends HandlebarsApplicationMixin(ApplicationV2) {
+
+  playerNumber: string | null;
+  characterNumber: string | null;
+  characterName: string | null;
+  currentFaction: string | null;
+
+  constructor(playerNumber: string | null, characterNumber: string | null, characterName: string | null, currentFaction: string | null, options: any = {}) {
+    super(options);
+    this.playerNumber = playerNumber;
+    this.characterNumber = characterNumber;
+    this.characterName = characterName;
+    this.currentFaction = currentFaction;
+  }
+
   static DEFAULT_OPTIONS = {
     id: "pfs-chronicle-generator",
     form: {
@@ -13,7 +24,6 @@ export class PFSChronicleGeneratorApp extends HandlebarsApplicationMixin(Applica
       closeOnSubmit: true,
     },
     position: {
-      width: 400,
     },
     tag: "form",
     window: {
@@ -36,10 +46,17 @@ export class PFSChronicleGeneratorApp extends HandlebarsApplicationMixin(Applica
     const data : any = foundry.utils.expandObject(formData.object);
 
     // Log the submitted values to the console
+    console.log("Submitted Player Number", data.playerNumber);
+    console.log("Submitted Character Number", data.characterNumber);
+    console.log("Submitted Character Name", data.characterName);
     console.log("Submitted GM Name:", data.gmName);
     console.log("Submitted GM PFS Number:", data.gmPfsNumber);
     console.log("Submitted Event Name:", data.eventName);
     console.log("Submitted Event Code:", data.eventCode);
+    console.log("Submitted XP Earned:", data.xpEarned);
+    console.log("Submitted Gold Earned:", data.goldEarned);
+    console.log("Submitted Notes:", data.notes);
+    console.log("Submitted Reputation:", data.reputation);
 
     // Here you would continue to process the data, like generating a PDF
   }
@@ -50,10 +67,17 @@ export class PFSChronicleGeneratorApp extends HandlebarsApplicationMixin(Applica
     const eventName = game.settings.get('pfs-chronicle-generator', 'eventName');
     const eventCode = game.settings.get('pfs-chronicle-generator', 'eventCode');
     return {
+      playerNumber: this.playerNumber,
+      characterNumber: this.characterNumber,
+      characterName: this.characterName,
       gmName,
       gmPfsNumber,
       eventName,
       eventCode,
+      xpEarned: 4,
+      goldEarned: 0,
+      notes: "",
+      reputation: this.currentFaction ? `${this.currentFaction}: +4` : "",
       buttons: [
         { type: "submit", icon: "fa-solid fa-save", label: "SETTINGS.Save" }
       ]
--- scripts/main.ts
+++ scripts/main.ts
@@ -44,13 +44,40 @@ Hooks.on('init', () => {
 
 });
 
-Hooks.on('getActorSheetHeaderButtons', (sheet: any, buttons: any) => {
-  buttons.unshift({
-    label: 'Generate Chronicle',
-    class: 'pfs-chronicle-generator-button',
-    icon: 'fas fa-file-pdf',
-    onclick: () => {
-      new PFSChronicleGeneratorApp().render({force: true});
-    },
-  });
+Hooks.on('renderCharacterSheetPF2e' as any, (sheet: any, html: any, data: any) => {
+    if (!game.user.isGM) return;
+    console.log("Rendering character sheet")
+    console.log(sheet.actor);
+    console.log(sheet.actor.system.pfs);
+    const playerNumber = sheet.actor.system.pfs.playerNumber;
+    const characterNumber = sheet.actor.system.pfs.characterNumber;
+    const characterName = sheet.actor.name;
+    const currentFaction = sheet.actor.system.pfs.currentFaction;
+    console.log("Player Number", playerNumber)
+    console.log("Character Number", characterNumber)
+    console.log("Character Name", characterName)
+
+    const pfsTab = html.find('.tab[data-tab="pfs"]');
+    if (pfsTab.length === 0) {
+        console.log("No PFS tab found")
+        return;
+    }
+
+    const header = document.createElement('header');
+    header.innerHTML = "PFS Chronicle Generator";
+    header.classList.add('pfs-chronicle-generator-header');
+    // Add data-group-id="pfs-chronicle-generator" as an attribute to the header
+    header.setAttribute('data-group-id', 'pfs-chronicle-generator');
+    const button = document.createElement('button');
+    button.innerHTML = '<section class="generate-chronicle"><i class="fas fa-file-pdf"></i> Generate Chronicle</section>';
+    button.classList.add('pfs-chronicle-generator-button');
+
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        new PFSChronicleGeneratorApp(playerNumber, characterNumber, characterName, currentFaction).render({force:true});
+    });
+
+    // append first the header, then the button, after each other
+    pfsTab.append(header);
+    pfsTab.append(button);
 });
